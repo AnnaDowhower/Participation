ff7a733d9a7f0b44aa0540e0c089bc34a87bd1fa
Anna Dowhower <anna.dowhower@att.net>
2021-03-25 17:19:32 -0500

    Done with Q2

---
+++ All types of collections/Question 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Generic.List<double> grades = new System.Collections.Generic.List<double>();
+
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Please enter your exam grade >>");
+                double grade = Convert.ToDouble(Console.ReadLine());
+                grades.Add(grade);
+
+                Console.WriteLine("Do you have another grade to enter? yes or no >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower()[0] == 'y');
+
+            double sum = 0, avg;
+
+            foreach (double grade in grades)
+            {
+                sum += grade;
+            }
+
+            avg = sum / grades.Count;
+            Console.WriteLine($"Your average grade is {avg.ToString("N2")}");
+
+        }
+    }
+}
